7524c665946d7a379e24bc673d2ace468c44c623
Sergey Chikunov <consull@outlook.com>
2024-04-19 00:45:39 +0500

    Третья задача из первого семинара по Csharp

---
+++ Task03/Program.cs
@@ -0,0 +1,36 @@
+﻿/* Напишите  программу,  которая  принимает  на  вход трёхзначное целое число и на выходе показывает 
+сумму первой и последней цифры этого числа.
+456 => 10
+782 => 9
+918 => 17
+if(a[0] == 'я') 
+int.Parse(NumStr01.ToString())
+int.Parse(NumStr03.ToString()) 
+string str = c.ToString();
+    string ex = s.ToString();
+
+    Console.WriteLine(Int32.Parse(str)); */
+
+Console.WriteLine("Введите целое трехзначное число: ");
+// int num = Convert.ToInt32(Console.ReadLine());
+
+string? NumStr = Console.ReadLine();
+char NumStr01 = NumStr[0];
+Console.WriteLine(NumStr01);
+char NumStr02 = NumStr[1];
+Console.WriteLine(NumStr02);
+char NumStr03 = NumStr[2];
+Console.WriteLine(NumStr03);
+
+string NumString01 = NumStr01.ToString();
+string NumString02 = NumStr03.ToString();
+
+int Num01 = Convert.ToInt32(NumString01);
+int Num02 = Convert.ToInt32(NumString01);
+
+Console.WriteLine(Num01);
+Console.WriteLine(Num02);
+
+int SumResult = Num01 + Num02;
+
+Console.WriteLine("Сумма первой и последней цифры введенного числа равна: ", SumResult);
