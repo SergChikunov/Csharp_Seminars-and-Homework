c56a449ffcd3509d7f23ec16ad71154d83a8cc27
Sergey Chikunov <consull@outlook.com>
2024-04-20 21:23:50 +0500

    Решение 4 ДЗ по Csharp, вывести четные числа до введенного значения

---
+++ HW005/Program.cs
@@ -0,0 +1,28 @@
+﻿int firstNumber;
+string? readNumOne = " ";
+bool isNumeric01 = false;
+int i = 2;
+
+do
+{
+    Console.WriteLine("Введите, нужно ввести именно число (!!!): ");
+    readNumOne = Console.ReadLine();
+
+    isNumeric01 = int.TryParse(readNumOne, out firstNumber); //проверка является ли ввод целым числом для первого числа, т.е. нам требуется значение True
+
+} while (isNumeric01 != true);
+
+
+firstNumber = Convert.ToInt32(readNumOne);
+
+Console.WriteLine("\n");
+Console.WriteLine(new string('=', 30)); //Console.WriteLine(new string('.', 10));
+Console.WriteLine(firstNumber); // Для отладки и наглядности
+Console.WriteLine(isNumeric01); // Для отладки и наглядности
+Console.WriteLine("\n");
+
+while (i <= firstNumber)
+{
+    Console.Write(i + " ");
+    i += 2;
+}
