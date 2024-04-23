7a2d9d96d69fb1a7013dec0774bfb9741844a4b0
Sergey Chikunov <consull@outlook.com>
2024-04-23 18:52:06 +0500

    ДЗ 2 по семинару 2, числа кратные 7 и 23

---
+++ HW006/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 1: Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 7 == 0 && num % 23 == 0 && num != 0)
+{
+    Console.WriteLine("Введено число кратное 7 и 23, это число: " + num);
+}
+else if (num == 0)
+{
+    Console.WriteLine("Введен 0");
+}
+else
+{
+    Console.WriteLine("Введено число не кратное 7 и 23, это число: " + num);
+}
