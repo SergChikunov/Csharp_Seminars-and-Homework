0dcf0d8b15fcbb83a55968046aa8bc98de7c9423
Sergey Chikunov <consull@outlook.com>
2024-04-23 19:05:14 +0500

    ДЗ 2 по семинару 2, задача по определению координат точки

---
+++ HW007/Program.cs
@@ -0,0 +1,30 @@
+﻿/* Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y), 
+причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка. */
+
+Console.Write("Введите числовую координату на оси X: ");
+int Xcoordinate = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите числовую координату на оси Y: ");
+int Ycoordinate = Convert.ToInt32(Console.ReadLine());
+
+
+if (Xcoordinate > 0 && Ycoordinate > 0)
+{
+    Console.WriteLine("Точка с такими координатами расположена в первой четверти окружности.");
+}
+else if (Xcoordinate < 0 && Ycoordinate > 0)
+{
+    Console.WriteLine("Точка с такими координатами расположена во второй четверти окружности.");
+}
+else if (Xcoordinate < 0 && Ycoordinate < 0)
+{
+    Console.WriteLine("Точка с такими координатами расположена в третьей четверти окружности.");
+}
+else if (Xcoordinate > 0 && Ycoordinate < 0)
+{
+    Console.WriteLine("Точка с такими координатами расположена в четвертой четверти окружности.");
+}
+else
+{
+    Console.WriteLine("Указаны некорректные координаты точки!");
+}
