c4dee0c61e88102a16638e4e355d524385980e0c
Sergey Chikunov <consull@outlook.com>
2024-04-23 19:23:48 +0500

    ДЗ 2 по семинару 2, определение наибольшей цифры во введенном числе

---
+++ HW008/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 3: Напишите программу, которая принимает на вход целое число из отрезка [10, 99] и показывает наибольшую цифру числа.
+
+int num;
+
+do
+{
+    Console.WriteLine("Введите целое число из диапазона от 10 до 99: ");
+    num = Convert.ToInt32(Console.ReadLine());
+} while (num < 10 || num > 99);
+
+int firstDigit = num / 10;
+int secondDigit = num % 10;
+
+if (firstDigit > secondDigit)
+{
+    Console.WriteLine("Наибольшая цифра в веденном числе: " + firstDigit);
+}
+else
+{
+    Console.WriteLine("Наибольшая цифра в веденном числе: " + secondDigit);
+}
