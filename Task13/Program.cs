9d5625ea1242d9fc9546f6225a59b88a137602ee
Sergey Chikunov <consull@outlook.com>
2024-04-26 00:01:48 +0500

    Семинар 2 Задача 4. Массивы. Трехзначное число в массив.

---
+++ Task13/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Дано натуральное трёхзначное число. Создайте массив, состоящий из цифр этого числа. Младший разряд 
+числа должен располагаться на 0- м индексе массива, старший – на 2-м.
+Пример
+456 => [6 5 4]
+781 => [1 8 7] */
+
+Console.WriteLine("Введите трехзначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[3];
+int count = 2;
+while (num != 0)
+{
+    arr[count] = num % 10;
+    num /= 10;
+    count--;
+}
+for(int i = 0; i < arr.Length; i++)
+{
+    Console.Write(arr[i] + " ");
+}
