17ffea4a27408af6fade9b2211195454e3db1ade
Sergey Chikunov <consull@outlook.com>
2024-05-21 08:16:46 +0500

    Семинар 6. Задание 3. Поиск гласных букв в строке

---
+++ Task23/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+string strValue = "поиск гласных букв в строке";
+char[] vowelsLellers = new char[] {'а','е','ё','и','о','у','ы','э','ю','я'};
+string tmpValue = "";
+int count = 0;
+for(int i = 0; i < strValue.Length; i++)
+{
+    if(vowelsLellers.Contains(strValue[i])) //метод Contains определяет, содержится ли символ strValue[i] в списке гласных букв
+    {
+        tmpValue = tmpValue + strValue[i];
+        count++;
+    }
+}
+
+Console.WriteLine();
+Console.Write("Исходная строка: " + strValue);
+Console.WriteLine();
+Console.WriteLine(new string('=', 70));
+Console.Write("Найдено гласных букв в исходной строке: " + count);
+Console.WriteLine();
+Console.WriteLine(new string('=', 70));
+Console.Write("Найденные гласные в строке, в порядке появления в исходной строке: " + tmpValue);
+Console.WriteLine();
