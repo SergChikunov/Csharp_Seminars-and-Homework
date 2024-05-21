3cef001aa059ac64cfa5d2af0b5f7b7b786246cb
Sergey Chikunov <consull@outlook.com>
2024-05-21 09:01:53 +0500

    Семинар 6. ДЗ задача 1.Создание строки из двумерного массива

---
+++ HW021/Program.cs
@@ -0,0 +1,35 @@
+﻿/*ДЗ семинар 6. Задача 1: Задайте двумерный массив символов (тип char [,]). Создать строку из символов этого массива.*/
+
+
+string CreateStringFromCharArray(char[,] matrix)
+{
+    string reslultString = "";
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            reslultString += matrix[i, j];
+        }
+    }
+    return reslultString;
+}
+
+void PrintMatrix(char[,] matrix) //печать двумерного массива
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j],5}");
+        }
+        Console.WriteLine(" |");
+    }
+}
+
+char[,] table = { { 'a', 'b', 'c', 'd' }, { 'e', 'f', 'g', 'h' } };
+Console.WriteLine("Исходный массив символов: ");
+PrintMatrix(table);
+string stringFromArray = CreateStringFromCharArray(table);
+Console.WriteLine();
+Console.Write("Строка из символов массива: " + stringFromArray);
