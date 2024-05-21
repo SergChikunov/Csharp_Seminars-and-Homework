8c27ae25f84e46a95779846b9ba5fd69a216aa8b
Sergey Chikunov <consull@outlook.com>
2024-05-21 09:20:42 +0500

    Семинар 6. ДЗ задача 2. Перевод строки из символов в нижний регистр

---
+++ HW022/Program.cs
@@ -0,0 +1,38 @@
+﻿/*ДЗ семинар 6. Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. Сформируйте строку, в которой все заглавные буквы заменены на строчные.*/
+
+/* В этом варианте посимвольный перевод строки в нижний регистр
+string UpperToLower(string str)
+{
+    string strResult = string.Empty;
+
+    for (int i = 0; i < str.Length; i++)
+    {
+        if (char.IsUpper(str[i]))
+        {
+            strResult += char.ToLower(str[i]);
+        }
+        else
+        {
+            strResult += str[i];
+        }
+    }
+    return strResult;
+}
+
+
+string str = "aBcD1ef!-";
+string strResult = UpperToLower(str);
+Console.WriteLine(strResult);
+*/
+
+string GetStringFromConsole() // в этом варианте перевод строки в нижний регистр используя встроенный метод в C#
+{
+    Console.WriteLine("Введите строку, которая содержит латинские буквы в обеих регистрах! Затем нажмите Enter...");
+    string inputStr = Console.ReadLine();
+    return inputStr;
+}
+
+string resultStr = GetStringFromConsole();
+Console.Write("Введенная строка: " + resultStr);
+resultStr = resultStr.ToLower();
+Console.Write("\nСтрока после обработки кода: " + resultStr);
